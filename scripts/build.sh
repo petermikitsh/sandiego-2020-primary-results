4ded25a5c3a0b605e4fdd2ca177a6700069cb36e
Peter Mikitsh <peter.mikitsh@gmail.com>
2020-03-07 11:50:42 -0800

    chore: generate consolidations.geojson

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,17 @@
-#!/bin/sh
+#!/bin/bash
+
+# Log Each command
+set -x
+
+# Bail on first error
+set -e
 
 SCRIPT_DIR=$(dirname $0)
 
 node $SCRIPT_DIR/build.js
+
+cat $SCRIPT_DIR/../data/sandiego.txt | \
+  npx --no-install mapshaper \
+  -i /dev/stdin \
+  -dissolve 'CONSNAME' \
+  -o $SCRIPT_DIR/../data/consolidations.geojson
