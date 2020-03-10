70fad9e8dadc62fbd0b595adb21c2eb30b7714c4
Peter Mikitsh <peter.mikitsh@gmail.com>
2020-03-09 22:35:36 -0700

    feat(App): dark mode

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
 import InputLabel from '@material-ui/core/InputLabel';
+import { makeStyles } from '@material-ui/core/styles';
 import FormControl from '@material-ui/core/FormControl';
 import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
@@ -12,10 +13,30 @@ import { capitalCase } from 'change-case';
 import styled from 'styled-components';
 import GithubCorner from 'react-github-corner';
 import { PrecinctMap } from './components/PrecinctMap';
+import useDarkMode from 'use-dark-mode';
+import { Theme } from '@material-ui/core';
+
+const LightMode = createMuiTheme({
+  palette: {
+    type: 'light',
+  },
+});
 
 const DarkMode = createMuiTheme({
   palette: {
     type: 'dark',
+    primary: {
+      light: '#757ce8',
+      main: '#90CAF9',
+      dark: '#002884',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
   },
 });
 
@@ -25,12 +46,30 @@ const StyledPage = styled.div`
 
   @media (min-width: 840px) {
     margin: 20px auto;
+    margin-bottom: 0;
   }
 `;
 
+const useStyles = makeStyles(theme => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+}));
+
 const App = () => {
+  const classes = useStyles();
+  const [regionLevel, setRegionLevel] = useState<string>('neighborhood');
   const [currContest, setCurrContest] = useState<string>();
   const [contests, setContests] = useState<object[]>([]);
+  const [theme, setTheme] = useState<Theme>();
+  const darkMode = useDarkMode();
+
+  useEffect(() => {
+    const darkModeEnabled = darkMode.value;
+    setTheme(darkModeEnabled ? DarkMode : LightMode);
+  }, [darkMode]);
+
   useEffect(() => {
     (async () => {
       // @ts-ignore
@@ -42,7 +81,7 @@ const App = () => {
   }, []);
 
   return (
-    <ThemeProvider theme={DarkMode}>
+    <ThemeProvider theme={theme}>
       <StyledPage>
         <GithubCorner href="https://github.com/petermikitsh/sandiego-2020-primary-results" />
         <Typography variant="h3" component="h1">
@@ -51,30 +90,68 @@ const App = () => {
         <Typography variant="h4" component="h2">
           March 3, 2020 Primary
         </Typography>
-        <FormControl style={{ margin: '20px 0' }}>
-          <InputLabel id="demo-simple-select-label">Contest</InputLabel>
-          {currContest && (
-            <Select
-              value={currContest}
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              onChange={e => {
-                // @ts-ignore
-                setCurrContest(e.target.value);
-              }}
-            >
-              {contests.map(contest => (
-                <MenuItem
-                  key={contest['Contest Name']}
-                  value={contest['Contest Name']}
-                >
-                  {capitalCase(contest['Contest Name'])}
-                </MenuItem>
-              ))}
-            </Select>
+        <FormControl className={classes.formControl}>
+          <InputLabel id="detail-level-label">Region</InputLabel>
+          <Select
+            value={regionLevel}
+            labelId="detail-level-label"
+            id="detail-level"
+            onChange={e => {
+              // @ts-ignore
+              setRegionLevel(e.target.value);
+            }}
+          >
+            <MenuItem value="precinct" disabled>
+              Precinct (coming soon)
+            </MenuItem>
+            <MenuItem value="neighborhood">Neighborhood</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl
+          className={classes.formControl}
+          style={{ maxWidth: '350px' }}
+        >
+          {currContest && contests && (
+            <>
+              <InputLabel id="demo-simple-select-label">Contest</InputLabel>
+              <Select
+                value={currContest}
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                onChange={e => {
+                  // @ts-ignore
+                  setCurrContest(e.target.value);
+                }}
+              >
+                {contests.map(contest => (
+                  <MenuItem
+                    key={contest['Contest Name']}
+                    value={contest['Contest Name']}
+                  >
+                    {capitalCase(contest['Contest Name'])}
+                  </MenuItem>
+                ))}
+              </Select>
+            </>
           )}
         </FormControl>
-        <Card variant="outlined" style={{ backgroundColor: '#333' }}>
+        <FormControl className={classes.formControl}>
+          <InputLabel id="mode-label">Mode</InputLabel>
+          <Select
+            value={String(darkMode.value)}
+            labelId="mode-label"
+            id="mode-select"
+            onChange={e => {
+              if (e.target.value !== String(darkMode.value)) {
+                darkMode.toggle();
+              }
+            }}
+          >
+            <MenuItem value="false">Light Mode</MenuItem>
+            <MenuItem value="true">Dark Mode</MenuItem>
+          </Select>
+        </FormControl>
+        <Card variant="outlined">
           <CardContent>
             <PrecinctMap contest={currContest} />
           </CardContent>
